817ba61443f1c0274f72313539845293bf598a93
TuGuapoHenryWood <101741266+TuGuapoHenryWood@users.noreply.github.com>
2023-04-24 20:08:03 -0500

    Add files via upload

---
+++ formFiller/action.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+
+Nombres y Apellidos: <?php echo $_POST["_n"]; ?><br>
+Fecha de Nacimiento: <?php echo $_POST["_f"]; ?><br>
+Ocupacion: <?php echo $_POST["_o"]; ?><br>
+Contacto: <?php echo $_POST["_c"]; ?><br>
+Nacionalidad: <?php echo $_POST["_d"]; ?><br>
+Nivel de Ingles: <?php echo $_POST["_i"]; ?><br>
+Lenguajes de Programacion: <?php echo $_POST["_l"]; ?><br>
+Aptitudes: <?php echo $_POST["_a"]; ?><br>
+Habilidades: <?php echo $_POST["_h"]; ?><br>
+Perfil: <?php echo $_POST["_p"]; ?><br>
+
+</body>
+</html>
